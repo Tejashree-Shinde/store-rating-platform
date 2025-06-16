a5a7a36e7aef50c75922c5b8c4a6b6bd1b4c90e1
Tejashree-Shinde <steju4396@gmail.com>
2025-06-17 01:46:57 +0530

    Add files via upload

---
+++ LoginPage.js
@@ -0,0 +1,27 @@
+import React, { useState } from "react";
+import API from "../services/api";
+
+export default function LoginPage() {
+  const [form, setForm] = useState({ email: "", password: "" });
+
+  const handleChange = (e) => setForm({ ...form, [e.target.name]: e.target.value });
+
+  const handleSubmit = async (e) => {
+    e.preventDefault();
+    try {
+      const res = await API.post("/auth/login", form);
+      localStorage.setItem("token", res.data.token);
+      alert("Logged in!");
+    } catch (err) {
+      alert(err.response.data.error);
+    }
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <input name="email" placeholder="Email" onChange={handleChange} />
+      <input name="password" placeholder="Password" type="password" onChange={handleChange} />
+      <button type="submit">Login</button>
+    </form>
+  );
+}
